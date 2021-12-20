870d3d4a0265f0d044bff7fffb09e487bc5be5e9
Christian Eiselt <dev@eiselt.ch>
2021-12-20 14:39:05 +0100

    remove scripts

--- buildAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# We want this to output $PATH without expansion
-# shellcheck disable=SC2016
-find . -name "AdventKit" -type d \
-	-exec echo Building {} \; \
-	-exec sh -c 'pushd . > /dev/null; cd "$1"; swift build -c release; popd > /dev/null' sh {} \;
