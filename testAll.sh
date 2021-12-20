84c7a44a2c6b7b79d3cf5adea93312e37eb03d90
christianeiselt <dev@eiselt.ch>
2021-12-20 14:44:26 +0100

    chg: use ubuntu-latest DO-169 (#1)
    * run on ubuntu-latest
    
    * remove -s for gnu find
    
    * remove scripts

--- testAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# We want this to output $PATH without expansion
-# shellcheck disable=SC2016
-find -s . -name "AdventKit" -type d \
-	-exec echo Running {} \; \
-	-exec sh -c 'pushd .  > /dev/null; cd "$1"; swift test  --enable-code-coverage --verbose; popd > /dev/null' sh {} \;
