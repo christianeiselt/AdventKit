f71ab25cbd60eac65ef2eeab13670d03d1b50c19
Christian Eiselt <dev@eiselt.ch>
2021-12-20 14:29:49 +0100

    remove -s for gnu find

--- buildAll.sh
+++ buildAll.sh
@@ -2,6 +2,6 @@
 
 # We want this to output $PATH without expansion
 # shellcheck disable=SC2016
-find -s . -name "AdventKit" -type d \
+find . -name "AdventKit" -type d \
 	-exec echo Building {} \; \
 	-exec sh -c 'pushd . > /dev/null; cd "$1"; swift build -c release; popd > /dev/null' sh {} \;
